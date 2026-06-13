621bc4d8e4f5f27add8355e578d4b567c21c40cb
Ben Milanko <ben.milanko@protonmail.com>
2026-06-13 17:15:43 +1000

    refactor: update endSoftMasked signature to include backdrop luminance and transfer parameters in tool classes

--- packages/pdf_graphics/tool/dump_glyph_bounds.dart
+++ packages/pdf_graphics/tool/dump_glyph_bounds.dart
@@ -94,5 +94,5 @@ class _GlyphBoundsDevice implements PdfDevice {
   void endSoftMasked(
       {required bool luminosity,
       required PdfRect backdrop,
-      required void Function() drawMask}) {}
+      required void Function() drawMask, double backdropLuminance = 0, double transferScale = 1, double transferOffset = 0}) {}
 }
